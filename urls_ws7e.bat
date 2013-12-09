46e164adff155173e20b60bab5af1fa11c57a5ff
taylor <tailor@u.washington.edu>
2013-12-09 08:29:26 -0800

    Removes wget logging
    Its easier to see the download status by looking at cmd.exe window
    instead of openning each wget log file.  This undoes commit 7734630.

--- urls_ws7e.bat
+++ urls_ws7e.bat
@@ -1,3 +1,3 @@
-wget.exe --output-file=%0.log --limit-rate=2m --directory-prefix="%CD%\.." --timestamping --input-file=urls_ws7e.txt
+wget.exe --limit-rate=2m --directory-prefix="%CD%\.." --timestamping --input-file=urls_ws7e.txt
 cd ..
 "Standard 7 SP1 32bit IBW.part1.exe" -s -d .
--- urls_ws7e_toolkit.bat
+++ urls_ws7e_toolkit.bat
@@ -1,3 +1,3 @@
-wget.exe --output-file=%0.log --limit-rate=2m --directory-prefix="%CD%\.." --timestamping --input-file=urls_ws7e_toolkit.txt
+wget.exe --limit-rate=2m --directory-prefix="%CD%\.." --timestamping --input-file=urls_ws7e_toolkit.txt
 cd ..
 "Standard 7 SP1 Toolkit.part01.exe" -s -d .
