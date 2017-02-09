ac7560697065b23a2b859e8303fef168f6e3a7c4
Taylor Monacelli <taylor.monacelli@streambox.com>
2017-02-08 22:09:49 -0800

    Writing to env:USERPROFILE\trash works

--- doit.ps1
+++ doit.ps1
@@ -6,7 +6,7 @@ if(!(test-path 'wget.exe')){
 
 # Windows Embedded Standard 7 Service Pack 1 Evaluation Edition
 Start-Job -ScriptBlock {
-	@'
+	$t = @'
 # Windows Embedded Standard 7 Service Pack 1 Evaluation Edition
 # http://www.microsoft.com/en-us/download/details.aspx?id=11887
 http://download.microsoft.com/download/1/B/5/1B5FDE63-DA91-4A22-A320-91E002DE1326/Standard_7SP1_64bit/Standard%207%20SP1%2064bit%20IBW.part1.exe
@@ -16,6 +16,8 @@ http://download.microsoft.com/download/1/B/5/1B5FDE63-DA91-4A22-A320-91E002DE132
 http://download.microsoft.com/download/1/B/5/1B5FDE63-DA91-4A22-A320-91E002DE1326/Standard_7SP1_64bit/Standard%207%20SP1%2064bit%20IBW.part5.rar
 http://download.microsoft.com/download/1/B/5/1B5FDE63-DA91-4A22-A320-91E002DE1326/Standard_7SP1_64bit/Standard%207%20SP1%2064bit%20IBW.part6.rar
 http://download.microsoft.com/download/1/B/5/1B5FDE63-DA91-4A22-A320-91E002DE1326/Standard_7SP1_64bit/Standard%207%20SP1%2064bit%20IBW.part7.rar
-'@ | Out-File -encoding ASCII urls_ws7e_64bit.txt
-
+'@
+	$outdir="$env:USERPROFILE\trash"
+	mkdir -force "$outdir"
+	set-content "$outdir\output.txt" -value $t
 }
