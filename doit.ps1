ce6a5b8d13334d2e64c9c0a8dba3bfc1d47713a4
Taylor Monacelli <taylor.monacelli@streambox.com>
2017-02-08 22:38:59 -0800

    working

--- doit.ps1
+++ doit.ps1
@@ -2,10 +2,13 @@ if(!(test-path 'wget.exe')){
 	Invoke-WebRequest 'http://installer-bin.streambox.com/wget.exe' -OutFile 'wget.exe'
 }
 
+$here = Split-Path -Parent $MyInvocation.MyCommand.Path
+
 ./wget --quiet --no-check-certificate --limit-rate=2m --directory-prefix=. --timestamping http://installer-bin.streambox.com/7za.exe
 
 # Windows Embedded Standard 7 Service Pack 1 Evaluation Edition
-Start-Job -ScriptBlock {
+$block = {
+	param($write_dir) "`$write_dir is $write_dir"
 	@'
 # Windows Embedded Standard 7 Service Pack 1 Evaluation Edition
 # http://www.microsoft.com/en-us/download/details.aspx?id=11887
@@ -16,5 +19,7 @@ http://download.microsoft.com/download/1/B/5/1B5FDE63-DA91-4A22-A320-91E002DE132
 http://download.microsoft.com/download/1/B/5/1B5FDE63-DA91-4A22-A320-91E002DE1326/Standard_7SP1_64bit/Standard%207%20SP1%2064bit%20IBW.part5.rar
 http://download.microsoft.com/download/1/B/5/1B5FDE63-DA91-4A22-A320-91E002DE1326/Standard_7SP1_64bit/Standard%207%20SP1%2064bit%20IBW.part6.rar
 http://download.microsoft.com/download/1/B/5/1B5FDE63-DA91-4A22-A320-91E002DE1326/Standard_7SP1_64bit/Standard%207%20SP1%2064bit%20IBW.part7.rar
-'@ | Out-File -encoding ASCII "$env:USERPROFILE\trash\urls_ws7e_64bit.txt"
+'@ | Out-File -encoding ASCII "$write_dir\urls_ws7e_64bit.txt"
 }
+
+Start-Job $block -Arg $here
