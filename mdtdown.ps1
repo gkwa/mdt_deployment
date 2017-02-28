2f48ab5cad81c9d9ec23f7cdcfff5334ea935575
Taylor Monacelli <taylor.monacelli@streambox.com>
2017-02-28 09:10:11 -0800

    Whitespace

--- mdtdown.ps1
+++ mdtdown.ps1
@@ -24,11 +24,11 @@ $jobs = @()
 # ##############################
 
 $j = {
-	param([string]$write_dir)
+    param([string]$write_dir)
 
-	set-location $write_dir
+    set-location $write_dir
 
-	@'
+    @'
 # Windows Embedded Standard 7 Service Pack 1 Evaluation Edition
 # http://www.microsoft.com/en-us/download/details.aspx?id=11887
 http://download.microsoft.com/download/1/B/5/1B5FDE63-DA91-4A22-A320-91E002DE1326/Standard_7SP1_64bit/Standard 7 SP1 64bit IBW.part1.exe
@@ -40,13 +40,12 @@ http://download.microsoft.com/download/1/B/5/1B5FDE63-DA91-4A22-A320-91E002DE132
 http://download.microsoft.com/download/1/B/5/1B5FDE63-DA91-4A22-A320-91E002DE1326/Standard_7SP1_64bit/Standard 7 SP1 64bit IBW.part7.rar
 '@ | Out-File -encoding ASCII "urls_ws7e_64bit.txt"
 
-	if(!(test-path "Standard 7 SP1 64bit IBW.iso"))
-	{
-		&"./wget.exe" --quiet --no-check-certificate --timestamping --limit-rate=2m `
+    if(!(test-path "Standard 7 SP1 64bit IBW.iso")) {
+        &"./wget.exe" --quiet --no-check-certificate --timestamping --limit-rate=2m `
 		  --directory-prefix=. --input-file="urls_ws7e_64bit.txt"
-		&rar x -y "$write_dir/Standard 7 SP1 64bit IBW.part1.exe" "$write_dir/"
-	}
-	&7z x -o"Standard 7 SP1 64bit IBW" "Standard 7 SP1 64bit IBW.iso"
+        &rar x -y "$write_dir/Standard 7 SP1 64bit IBW.part1.exe" "$write_dir/"
+    }
+    &7z x -o"Standard 7 SP1 64bit IBW" "Standard 7 SP1 64bit IBW.iso"
 }
 
 $jobs += $j
@@ -55,11 +54,11 @@ $jobs += $j
 
 # Windows Embedded Standard 7 Service Pack 1 Evaluation Edition
 $j = {
-	param([string]$write_dir)
+    param([string]$write_dir)
 
-	set-location $write_dir
+    set-location $write_dir
 
-	@'
+    @'
 # Windows Embedded Standard 7 Service Pack 1 Evaluation Edition
 # http://www.microsoft.com/en-us/download/details.aspx?id=11887
 http://download.microsoft.com/download/1/B/5/1B5FDE63-DA91-4A22-A320-91E002DE1326/Standard_7SP1_Toolkit/Standard 7 SP1 Toolkit.part01.exe
@@ -72,13 +71,12 @@ http://download.microsoft.com/download/1/B/5/1B5FDE63-DA91-4A22-A320-91E002DE132
 http://download.microsoft.com/download/1/B/5/1B5FDE63-DA91-4A22-A320-91E002DE1326/Standard_7SP1_Toolkit/Standard 7 SP1 Toolkit.part08.rar
 '@ | Out-File -encoding ASCII "urls_ws7e_toolkit.txt"
 
-	if(!(test-path "Standard 7 SP1 Toolkit.iso"))
-	{
-		&"./wget.exe" --quiet --no-check-certificate --timestamping --limit-rate=2m `
+    if(!(test-path "Standard 7 SP1 Toolkit.iso")) {
+        &"./wget.exe" --quiet --no-check-certificate --timestamping --limit-rate=2m `
 		  --directory-prefix=. --input-file="urls_ws7e_toolkit.txt"
-		&rar x -y "Standard 7 SP1 Toolkit.part01.exe" "$write_dir/"
-	}
-	&7z x -o"Standard 7 SP1 Toolkit" "Standard 7 SP1 Toolkit.iso"
+        &rar x -y "Standard 7 SP1 Toolkit.part01.exe" "$write_dir/"
+    }
+    &7z x -o"Standard 7 SP1 Toolkit" "Standard 7 SP1 Toolkit.iso"
 }
 
 $jobs += $j
@@ -88,28 +86,28 @@ $jobs += $j
 # Windows Assessment and Deployment Kit (Windows ADK) for Windows 10
 # http://msdn.microsoft.com/en-us/library/windows/hardware/hh825494.aspx#InstallingNonNetworked
 $j = {
-	param([string]$write_dir)
+    param([string]$write_dir)
 
-	set-location $write_dir
+    set-location $write_dir
 
-	@'
+    @'
 # Windows Assessment and Deployment Kit (Windows ADK) for Windows 10
 http://download.microsoft.com/download/9/A/E/9AE69DD5-BA93-44E0-864E-180F5E700AB4/adk/adksetup.exe
 '@ | Out-File -encoding ASCII "urls_adk.txt"
 
-	&"./wget.exe" --quiet --no-check-certificate --timestamping --limit-rate=2m `
+    &"./wget.exe" --quiet --no-check-certificate --timestamping --limit-rate=2m `
 	  --directory-prefix=. --input-file="urls_adk.txt"
-	&"$write_dir/adksetup.exe" /log "$write_dir/adksetup1.log" /quiet /features +
-	# you can't use $lastExitCode here, its always 0...maybe due to /quiet
-	$m=gc "adksetup1.log" | select-string -quiet -case 'ERROR:'
-	if($m.Count){
-		throw "adksetup.exe failed, see $write_dir/adksetup1.log"
-	}
-	&./adksetup.exe /log adksetup2.log /ceip on /quiet /features +
-	$m=gc adksetup2.log | select-string -quiet -case 'ERROR:'
-	if($m.Count){
-		throw "adksetup.exe failed, see $write_dir/adksetup2.log"
-	}
+    &"$write_dir/adksetup.exe" /log "$write_dir/adksetup1.log" /quiet /features +
+    # you can't use $lastExitCode here, its always 0...maybe due to /quiet
+    $m=gc "adksetup1.log" | select-string -quiet -case 'ERROR:'
+    if($m.Count){
+        throw "adksetup.exe failed, see $write_dir/adksetup1.log"
+    }
+    &./adksetup.exe /log adksetup2.log /ceip on /quiet /features +
+    $m=gc adksetup2.log | select-string -quiet -case 'ERROR:'
+    if($m.Count){
+        throw "adksetup.exe failed, see $write_dir/adksetup2.log"
+    }
 }
 
 $jobs += $j
@@ -118,11 +116,11 @@ $jobs += $j
 
 # Microsoft deployment toolkit
 $j = {
-	param([string]$write_dir)
+    param([string]$write_dir)
 
-	set-location $write_dir
+    set-location $write_dir
 
-	@'
+    @'
 http://download.microsoft.com/download/B/F/5/BF5DF779-ED74-4BEC-A07E-9EB25694C6BB/Whats New in MDT 2013 Guide.docx
 http://download.microsoft.com/download/B/F/5/BF5DF779-ED74-4BEC-A07E-9EB25694C6BB/MDT 2013 Documentation.zip
 http://download.microsoft.com/download/B/F/5/BF5DF779-ED74-4BEC-A07E-9EB25694C6BB/MDT 2013 Release Notes.docx
@@ -130,9 +128,9 @@ https://download.microsoft.com/download/3/3/9/339BE62D-B4B8-4956-B58D-73C4685FC4
 https://download.microsoft.com/download/3/3/9/339BE62D-B4B8-4956-B58D-73C4685FC492/MicrosoftDeploymentToolkit_x86.msi
 '@ | Out-File -encoding ASCII "urls_mdt.txt"
 
-	&"./wget.exe" --quiet --no-check-certificate --timestamping --limit-rate=2m `
+    &"./wget.exe" --quiet --no-check-certificate --timestamping --limit-rate=2m `
 	  --directory-prefix=. --input-file="urls_mdt.txt"
-	&7z x -y -o. "MDT 2013 Documentation.zip"
+    &7z x -y -o. "MDT 2013 Documentation.zip"
 }
 
 $jobs += $j
@@ -143,15 +141,15 @@ $jobs += $j
 # customized wedu installer wedu_defaults_install_v1.2.exe
 # FIXME: replace this nsis nonsense with powershell
 $j = {
-	param([string]$write_dir)
+    param([string]$write_dir)
 
-	set-location $write_dir
+    set-location $write_dir
 
-	@'
+    @'
 http://installer-bin.streambox.com/wedu_defaults_install_v1.2.exe
 '@ | Out-File -encoding ASCII "urls_wedu.txt"
 
-	&./wget.exe --quiet --no-check-certificate --timestamping --limit-rate=2m `
+    &./wget.exe --quiet --no-check-certificate --timestamping --limit-rate=2m `
 	  --directory-prefix=. --input-file="urls_wedu.txt"
 }
 
@@ -160,15 +158,15 @@ $jobs += $j
 # ##############################
 
 $j = {
-	param([string]$write_dir)
+    param([string]$write_dir)
 
-	set-location $write_dir
+    set-location $write_dir
 
-	@'
+    @'
 http://taylors-bucket.s3.amazonaws.com/win7_pro_oem.iso
 '@ | Out-File -encoding ASCII "urls_win7pro.txt"
 
-	&./wget.exe --quiet --no-check-certificate --timestamping --limit-rate=20m `
+    &./wget.exe --quiet --no-check-certificate --timestamping --limit-rate=20m `
 	  --directory-prefix=. --input-file="urls_win7pro.txt"
 }
 
@@ -177,17 +175,17 @@ $jobs += $j
 # ##############################
 
 $j = {
-	param([string]$write_dir)
+    param([string]$write_dir)
 
-	set-location $write_dir
+    set-location $write_dir
 
-	@'
+    @'
 http://taylors-bucket.s3.amazonaws.com/WS7P_2013-12-09-1641.wim
 http://taylors-bucket.s3.amazonaws.com/WS7P_2014-01-01-1045.wim
 http://taylors-bucket.s3.amazonaws.com/mdt.7z
 '@ | Out-File -encoding ASCII "urls_mdt_taylor_made.txt"
 
-	&./wget.exe --quiet --no-check-certificate --timestamping --limit-rate=20m `
+    &./wget.exe --quiet --no-check-certificate --timestamping --limit-rate=20m `
 	  --directory-prefix=. --input-file="urls_mdt_taylor_made.txt"
 }
 
@@ -197,11 +195,11 @@ $jobs += $j
 
 # Windows Embedded Standard 7 Service Pack 1 Evaluation Edition
 $j = {
-	param([string]$write_dir)
+    param([string]$write_dir)
 
-	set-location $write_dir
+    set-location $write_dir
 
-	@'
+    @'
 # Windows Embedded Standard 7 Service Pack 1 Evaluation Edition
 # http://www.microsoft.com/en-us/download/details.aspx?id=11887
 http://download.microsoft.com/download/1/B/5/1B5FDE63-DA91-4A22-A320-91E002DE1326/Standard_7SP1_32bit/Standard 7 SP1 32bit IBW.part1.exe
@@ -211,13 +209,12 @@ http://download.microsoft.com/download/1/B/5/1B5FDE63-DA91-4A22-A320-91E002DE132
 http://download.microsoft.com/download/1/B/5/1B5FDE63-DA91-4A22-A320-91E002DE1326/Standard_7SP1_32bit/Standard 7 SP1 32bit IBW.part5.rar
 '@ | Out-File -encoding ASCII "urls_ws7e.txt"
 
-	if(!(test-path "Standard 7 SP1 32bit IBW.iso"))
-	{
-		&./wget.exe --quiet --no-check-certificate --timestamping --limit-rate=2m `
+    if(!(test-path "Standard 7 SP1 32bit IBW.iso")) {
+        &./wget.exe --quiet --no-check-certificate --timestamping --limit-rate=2m `
 		  --directory-prefix=. --input-file="urls_ws7e.txt"
-		&rar x -y "Standard 7 SP1 32bit IBW.part1.exe" "$write_dir/"
-	}
-	&7z x -o"Standard 7 SP1 32bit IBW" "Standard 7 SP1 32bit IBW.iso"
+        &rar x -y "Standard 7 SP1 32bit IBW.part1.exe" "$write_dir/"
+    }
+    &7z x -o"Standard 7 SP1 32bit IBW" "Standard 7 SP1 32bit IBW.iso"
 }
 
 $jobs += $j
@@ -236,11 +233,10 @@ $path = gci -ea 0 "${env:SYSTEMDRIVE}/Prog*/7-zip/7z.exe" | select -first 1 | se
 Install-BinFile -Path $path -Name 7z
 
 if(!(test-path wget.exe)){
-	Invoke-WebRequest -Uri 'http://installer-bin.streambox.com/wget.exe' -OutFile 'wget.exe'
+    Invoke-WebRequest -Uri 'http://installer-bin.streambox.com/wget.exe' -OutFile 'wget.exe'
 }
 
 $script_dir_base = Split-Path -Parent $MyInvocation.MyCommand.Path
-foreach($job in $jobs)
-{
-	Start-Job $job -Arg $script_dir_base
+foreach($job in $jobs) {
+    Start-Job $job -Arg $script_dir_base
 }
